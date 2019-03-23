5da1ea3a39c449196befacc4040db680180768c6
Nicolai Sticht <nsticht@livinglogic.de>
2019-03-23 11:49:31 +0100

    Create initial folder structure

---
+++ lib/simple_token_generator.rb
@@ -0,0 +1,6 @@
+require "simple_token_generator/version"
+
+module SimpleTokenGenerator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/simple_token_generator/version.rb
@@ -0,0 +1,3 @@
+module SimpleTokenGenerator
+  VERSION = "0.1.0"
+end
